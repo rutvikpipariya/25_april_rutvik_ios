8a68638ab1629d5e3c9f72e9b2508a0ba22e3008
Rutvik Pipaliya <125720851+rutvikpipariya@users.noreply.github.com>
2023-05-16 12:55:59 +0530

    For_in.swift

---
+++ Practice /For_in.swift
@@ -0,0 +1,26 @@
+/*
+import Foundation
+for num in 1...10
+      { 
+              print(num)
+       }
+
+
+import Foundation
+var num = ["1","2","3","4","5","6","7","8","9","10"]
+for num in num
+        {
+                print(num)
+        }
+
+*/
+import Foundation
+for week in 1...2 
+{
+print("Week: \(week)")
+for day in ["Sunday", "Monday","Tuesday", "Wednesday", "Thursday", "Friday","Saturday"]
+{
+print(" Day: \(day)")
+} 
+print(" ")
+}
