b38f0cadfd328db238232b8d067025f197e90c83
Rutvik Pipaliya <125720851+rutvikpipariya@users.noreply.github.com>
2023-05-16 13:10:16 +0530

    Sumof2num.swift

---
+++ Practice /Sumof2num.swift
@@ -0,0 +1,6 @@
+import Foundation
+print(" Enter the first value : ")
+var value1 = Int(readLine()!)!
+print(" Enter the second value : ")
+var value2 = Int(readLine()!)!
+print("The sum of two value is :  \(value1 + value2)") 
