a4a2cebe9eb2e961e52f4e7922050b11d8abc9b4
Rutvik Pipaliya <125720851+rutvikpipariya@users.noreply.github.com>
2023-06-03 11:46:15 +0530

    Arithmeticoperation.swift

---
+++ Assignment /Module 2 /Arithmeticoperation.swift
@@ -0,0 +1,13 @@
+import Foundation
+print("Enter first number :") 
+var a = Float(readLine()!)!
+print("Enter second number :") 
+var b = Float(readLine()!)!
+var addition = a + b
+var subtraction a - b
+var multiplication = a * b
+var division = a / b
+print("Addition of A and B : \(addition)")
+print("Subtraction of A and B : \(subtraction)")
+print("Multiplication of A and B : \(multiplication)")
+print("Division of A and B : \(division)")
