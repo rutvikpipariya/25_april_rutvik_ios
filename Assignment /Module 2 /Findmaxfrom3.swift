f38ea8e116c371c6c52e7360f8c66b97b6975800
Rutvik Pipaliya <125720851+rutvikpipariya@users.noreply.github.com>
2023-06-03 11:45:07 +0530

    Findmaxfrom3.swift

---
+++ Assignment /Module 2 /Findmaxfrom3.swift
@@ -0,0 +1,17 @@
+import Foundation
+print("Enter first number :")
+var num1 = Int(readLine()!)!
+print("Enter second number :")
+var num2 = Int(readLine()!)!
+print("Enter third number :")
+var num3 = Int(readLine()!)!
+if num1>num2
+{
+print("num1 is max")
+} else if num2>num3
+{
+print("num2 is max")
+} else if num3>num1
+{
+print("num3 is max")
+}
