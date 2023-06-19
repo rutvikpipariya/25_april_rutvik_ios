ba2eadd5ebb7aaa2ec39ef9ab57fd93cfe9d0cb2
Rutvik Pipaliya <125720851+rutvikpipariya@users.noreply.github.com>
2023-06-19 09:59:16 +0530

    Create Module 2 no 20.swift

---
+++ Assignment /Module 2 /Module 2 no 20.swift
@@ -0,0 +1,15 @@
+import Foundation
+var a:String! = "Username"
+var b:String! = "password"
+print("Enter Username:")
+var xa = readLine()!
+print("Enter Password")
+var xb = readLine()!
+if a == xa
+{
+    print("Login Successfully")
+}
+else
+{
+    print("Try again...")
+}
