c98e4177cbab72cd53609c1a6c7b16b8eda74475
Rutvik Pipaliya <125720851+rutvikpipariya@users.noreply.github.com>
2023-06-19 10:17:43 +0530

    Create Module 2 no 22.swift

---
+++ Assignment /Module 2 /Module 2 no 22.swift
@@ -0,0 +1,6 @@
+import Foundation
+var strarray = ["duniya", "gol", "hai"]
+strarray.sort()
+print("Sorted array using Method: \(strarray)")
+let srtarray = strarray.sorted()
+print("Sorted array using Function: \(srtarray)")
