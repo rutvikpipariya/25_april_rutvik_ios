010c54b8ff1bf46f083a376d5f28e6be85ce4f9a
Rutvik Pipaliya <125720851+rutvikpipariya@users.noreply.github.com>
2023-06-19 09:52:59 +0530

    Create Module 2 no 19.swift

---
+++ Assignment /Module 2 /Module 2 no 19.swift
@@ -0,0 +1,14 @@
+import Foundation
+let a: [Int] = [151,152,153,154,155,156,157,158,159,160]
+if !a.isEmpty
+{
+    print("Array has elements:")
+    for i in a 
+    {
+        print(i)
+    }
+} 
+else 
+{
+    print("empty array")
+}
