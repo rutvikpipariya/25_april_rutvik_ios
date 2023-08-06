ac6bbc9846d2bf86a834f1173693f9038562221b
Rutvik Pipaliya <125720851+rutvikpipariya@users.noreply.github.com>
2023-08-06 17:09:43 +0530

    Add files via upload

---
+++ Assignment /module 5/dynamicTool-Bar/dynamicTool-Bar/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  dynamicTool-Bar
+//
+//  Created by JADEJA PARIKSHITSINH on 06/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func button1(_ sender: Any)
+    {
+        let alert=UIAlertController(title: "Welcome", message: "This is Dynamic Toolbar and Navigationbar", preferredStyle: .alert)
+        let ok=UIAlertAction(title: "OK", style: .default, handler: nil)
+        let more=UIAlertAction(title: "MORE", style: .destructive, handler: nil)
+        alert.addAction(ok)
+        alert.addAction(more)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func button2(_ sender: Any)
+    {
+        let photo=UIImagePickerController()
+        present(photo, animated: true, completion: nil)
+    }
+    @IBAction func button3(_ sender: Any)
+    {
+        let alert=UIAlertController(title: "Create Folder", message: "Your Folder Name", preferredStyle: .alert)
+        alert.addTextField { (txt) in
+            
+        }
+        let ok=UIAlertAction(title: "Cancel", style: .default, handler: nil)
+        let more=UIAlertAction(title: "Create", style: .destructive, handler: nil)
+        alert.addAction(ok)
+        alert.addAction(more)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func button4(_ sender: Any)
+    {
+        let alert=UIAlertController(title: "Coin", message: "Add Coins", preferredStyle:.actionSheet)
+        let btn1=UIAlertAction(title: "100+", style: .default, handler: nil)
+        let btn2=UIAlertAction(title: "1000+", style: .destructive, handler: nil)
+        let btn3=UIAlertAction(title: "2000+", style: .destructive, handler: nil)
+        let btn4=UIAlertAction(title: "3000+", style: .destructive, handler: nil)
+        alert.addAction(btn1)
+        alert.addAction(btn2)
+        alert.addAction(btn3)
+        alert.addAction(btn4)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func button5(_ sender: Any)
+    {
+        view.backgroundColor=UIColor.red
+    }
+}
+
